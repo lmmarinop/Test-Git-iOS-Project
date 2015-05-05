e9b23b5dcccf4a7bc1caae408576eb448210185b
Mauricio <mauricio@MacBook-Pro-de-Mauricio.local>
2015-05-05 13:48:47 -0500

    added a test class

---
+++ GIt Test/Test.h
@@ -0,0 +1,13 @@
+//
+//  Test.h
+//  GIt Test
+//
+//  Created by Mauricio on 5/5/15.
+//  Copyright (c) 2015 Mauricio. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Test : NSObject
+
+@end
---
+++ GIt Test/Test.m
@@ -0,0 +1,13 @@
+//
+//  Test.m
+//  GIt Test
+//
+//  Created by Mauricio on 5/5/15.
+//  Copyright (c) 2015 Mauricio. All rights reserved.
+//
+
+#import "Test.h"
+
+@implementation Test
+
+@end
